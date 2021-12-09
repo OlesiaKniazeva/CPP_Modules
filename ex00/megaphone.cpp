bb13190d014654d34ceecc80022b35e20add4eb3
Madeleine Yael <myael@student.21-school.ru>
2021-12-10 01:03:33 +0300

    add ex00 - megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,6 +2,27 @@
 
 int main(int argc, char **argv)
 {
-	(void)argc;
-	std::cout << argv[1] << "\n";
+    int i;
+    int g;
+
+    i = 1;
+    if (argc < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        while (argv[i])
+        {
+            g = 0;
+            while (argv[i][g])
+            {
+                std::cout << (char)toupper(argv[i][g]);
+                g++;
+            }
+            if (argv[i + 1])
+                std::cout << " ";
+            else
+                std::cout << std::endl;
+            i++;
+        }
+    }
 }
