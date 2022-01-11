96dadc87e6762e493af24215cff991a0b0fd530d
Madeleine Yael <myael@student.21-school.ru>
2022-01-11 19:57:51 +0300

    start of making task

--- Day01/ex04/main.cpp
+++ Day01/ex04/main.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
 
-
-int main()
+int main(int argc, char **argv)
 {
-
+  if (argc != 4) {
+    std::cout << "\033[4mEnter filename and two strings: \033[0m" << std::endl
+              << std::endl
+              << "Example: \033[33m./replace FILENAME STRING STRING\033[39m" << std::endl;
+    return (0);
+  }
+  std::string filename, s1, s2;
+  filename = argv[0];
+  s1 = argv[1];
+  s2 = argv[2];
 }
