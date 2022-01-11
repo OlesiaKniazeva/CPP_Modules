653835bd836d5d275ad4905742ee4cd81f81af97
Madeleine Yael <myael@student.21-school.ru>
2022-01-11 19:41:44 +0300

    add class for Weapon and it's realisation

--- Day01/ex03/Weapon.cpp
+++ Day01/ex03/Weapon.cpp
@@ -1,1 +1,12 @@
+#include "Weapon.hpp"
 
+Weapon::Weapon( const std::string& weapon ) : weapon_(weapon) {
+}
+
+const std::string& Weapon::getType( void ) const {
+  return (weapon_);
+}
+
+void Weapon::setType( const std::string& weapon_type ) {
+  weapon_ = weapon_type;
+}
--- Day01/ex03/Weapon.hpp
+++ Day01/ex03/Weapon.hpp
@@ -1,5 +1,18 @@
 #ifndef WEAPON_HPP
 # define WEAPON_HPP
 
+# include <iostream>
+class Weapon {
+
+  public:
+    Weapon( const std::string& weapon );
+
+    const std::string& getType( void ) const;
+    void setType( const std::string& weapon_type );
+
+  private:
+    std::string weapon_;
+
+};
 
 #endif
