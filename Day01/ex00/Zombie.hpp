2d4c15368fc357b4db7d1573c2d793900cfa7838
Madeleine Yael <myael@student.21-school.ru>
2022-01-11 17:26:58 +0300

    add Zombie.hpp class

---
+++ Day01/ex00/Zombie.hpp
@@ -0,0 +1,24 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie {
+
+  public:
+
+  Zombie( const std::string& name );
+  ~Zombie( void );
+
+  void announce( void ) const;
+
+  private:
+
+  Zombie ( void );
+  std::string zombie_name_;
+};
+
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+
+#endif
