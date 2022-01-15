f69f2904d3ef20cf1df1ee4d0c02fbe76eede9b3
Madeleine Yael <myael@student.21-school.ru>
2022-01-15 19:05:09 +0300

    fix segfault if variable is uninitialised

--- Day01/ex03/HumanB.cpp
+++ Day01/ex03/HumanB.cpp
@@ -1,7 +1,7 @@
 #include "HumanB.hpp"
 
 HumanB::HumanB( const std::string& name )
-  : human_name_(name) {
+  : human_weapon_(NULL), human_name_(name) {
 }
 
 const std::string& HumanB::getWeapon( void ) const {
@@ -13,6 +13,11 @@ void  HumanB::setWeapon( Weapon& new_weapon ) {
 }
 
 void  HumanB::attack( void ) {
-  std::cout << human_name_ << " attacks with his "
-            << getWeapon() << std::endl;
+  if (human_weapon_) {
+    std::cout << human_name_ << " attacks with his "
+              << getWeapon() << std::endl;
+  }
+  else {
+    std::cout << human_name_ << " is disarmed" << std::endl;
+  }
 }
