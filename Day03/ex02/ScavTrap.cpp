45b2815fa11d3e81ca7631a62ad69cddb45676b0
Madeleine Yael <myael@student.21-school.ru>
2022-01-21 20:49:09 +0300

    add realisation of ex02 Day03

---
+++ Day03/ex02/ScavTrap.cpp
@@ -0,0 +1,36 @@
+#include "ScavTrap.hpp"
+
+void ScavTrap::SetDefaultPoints() {
+  hitpoints_ = 100;
+  energy_points_ = 50;
+  attack_damage_ = 20;
+   std::cout << "\033[35m    It has: " << hitpoints_ << " hitpoints, "
+            << energy_points_ << " energy points" << std::endl
+            << "    It can cause: " << attack_damage_
+            << " points of damage \033[39m" << std::endl;
+}
+
+ScavTrap::ScavTrap(const std::string & name) : ClapTrap(name) {
+    std::cout << "ScavTrap " << name_ << " is created" << std::endl;
+    SetDefaultPoints();
+}
+
+ScavTrap::~ScavTrap() {
+    std::cout << "ScavTrap " << name_ << " destroyed" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap & rhs) {
+  *this = rhs;
+}
+
+ScavTrap& ScavTrap::operator=(ScavTrap const & rhs) {
+  name_ = rhs.name_;
+  hitpoints_ = rhs.hitpoints_;
+  energy_points_ = rhs.energy_points_;
+  attack_damage_ = rhs.attack_damage_;
+  return (*this);
+}
+
+void ScavTrap::guardGate() {
+  std::cout << "\033[36m    " << name_ << " is staying on Guard Gate\033[39m" << std::endl;
+}
---
+++ Day03/ex02/ScavTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef SCAV_TRAP_HPP
+# define SCAV_TRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+
+  public:
+    ScavTrap(const std::string & name);
+    ScavTrap(const ScavTrap & rhs);
+    ~ScavTrap();
+
+    ScavTrap& operator=(ScavTrap const & rhs);
+
+    void guardGate();
+
+  protected:
+    ScavTrap();
+
+  private:
+    void SetDefaultPoints();
+
+
+};
+
+#endif
