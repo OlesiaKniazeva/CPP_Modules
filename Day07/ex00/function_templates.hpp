a58886b729aa7cc90fa367cc77b0c043fddc40ae
Madeleine Yael <myael@student.21-school.ru>
2022-02-03 13:16:43 +0300

    add realisation of ex00 Day07

---
+++ Day07/ex00/function_templates.hpp
@@ -0,0 +1,24 @@
+#ifndef FUNCTION_TEMPLATES_HPP
+# define FUNCTION_TEMPLATES_HPP
+
+#include <iostream>
+
+template<typename T>
+  void swap(T& x, T& y) {
+    T temp;
+    temp = x;
+    x = y;
+    y = temp;
+  }
+
+template<typename T>
+const T & min( const T & x, const T & y) {
+  return (x < y) ? x : y;
+}
+
+template<typename T>
+const T & max( const T & x, const T & y) {
+  return (x > y) ? x : y;
+}
+
+#endif
